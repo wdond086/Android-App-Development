5ba9d11c7cde691609afe9d85dbbc6e04fada933
wdond086 <tdondjiwilfried@hotmail.com>
2018-09-06 15:59:33 -0400

    Learned to use Toasts to display messages.

--- DemoApp/app/src/main/java/com/tdondjiwilfried/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/tdondjiwilfried/demoapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +20,12 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void logFunction(View view) {
+        // Used to get information from the user. To access that info we use an EditText
         EditText logInfo = findViewById(R.id.logInfo);
+        // To post a small message for a certain amount of time we use a Toast.
+        // Toast's makeText function requires the context(The page where to display it)
+        // the text to be displayed, and the length of display time.
+        Toast.makeText(MainActivity.this, logInfo.getText().toString(), Toast.LENGTH_LONG).show();
         Log.i("Info", logInfo.getText().toString());
     }
 }
---
+++ ToastDemo/app/src/main/java/com/tdondjiwilfried/toastdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tdondjiwilfried.toastdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayHi(View view) {
+        EditText name = findViewById(R.id.name);
+        Toast.makeText(MainActivity.this, "Hi there, " + name.getText().toString() +
+            "!", Toast.LENGTH_LONG).show();
+    }
+}
